ed81b4a3c7c85f9686bc6ba86866b51c88377fe8
rusticwine <Sisavusa1>
2018-12-16 16:40:40 +0100

    added some JS shit (still not working)

--- src/main/java/com/sisa/graphql/spqr/demo/ApplicationWebConfig.java
+++ src/main/java/com/sisa/graphql/spqr/demo/ApplicationWebConfig.java
@@ -9,7 +9,7 @@ public class ApplicationWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/graphiql").setViewName(
+        registry.addViewController("/static/graphiql").setViewName(
                 "forward:/graphiql/index.html");
     }
 }
